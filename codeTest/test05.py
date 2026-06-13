b4ad5ef39e6220daa9026905543d20dce3482640
Edison <3078601436@qq.com>
2026-06-13 22:15:25 +0800

    add: wait_until

--- codeTest/test05.py
+++ codeTest/test05.py
@@ -1,4 +1,5 @@
 from pywinauto.application import Application
+from pywinauto.timings import wait_until
 
 # # 创建Application对象，连接到正在运行的Typora进程
 # app = Application(backend='uia').connect(process=5092)
@@ -79,18 +80,42 @@ from pywinauto.application import Application
 # proc_chid = win.child_window(auto_id="PaneTitleTextBlock", control_type="Text")
 # proc_chid.wait("ready") # 等待失败
 
-# 打开计算器
-app = Application(backend="uia").connect(process=14752)
-win = app.window(title="计算器")
+# # 打开计算器
+# app = Application(backend="uia").connect(process=14752)
+# win = app.window(title="计算器")
+#
+# # 把焦点放到计算器上
+# win.set_focus()
+#
+# # 先对计算器进行操作: 输入1
+# num1_btn = win.child_window(title="一", auto_id="num1Button", control_type="Button")
+#
+# # 点击按钮
+# num1_btn.click_input()
+#
+# # 等待成功
+# win.wait("active")
 
-# 把焦点放到计算器上
-win.set_focus()
+# i = 0
+# def work():
+#     global i
+#     i += 1
+#     print("当前i的值为", i)
+#     return i
+#
+# # 等待work返回的结果为5，继续往下执行
+# wait_until(10, 1, work, 5)
+# print("等待通过")
 
-# 先对计算器进行操作: 输入1
-num1_btn = win.child_window(title="一", auto_id="num1Button", control_type="Button")
 
-# 点击按钮
-num1_btn.click_input()
+def get_window():
+    app = Application(backend="uia").connect(process=14752)
+    win = app.window(title="计算器")
+    return win.is_visible() # 验证窗口是否为可见状态(必须打开窗口)
 
-# 等待成功
-win.wait("active")
+def test_wait():
+    # 5s钟内等待get_window方法返回结果为True
+    wait_until(5, 2, get_window, True)
+    print("等待通过")
+
+test_wait()
