078ef4d88da13701b4eaff7eb4756783129c7f7b
Edison <3078601436@qq.com>
2026-06-13 21:35:48 +0800

    add: exists 和 visible

--- codeTest/test05.py
+++ codeTest/test05.py
@@ -1,19 +1,37 @@
 from pywinauto.application import Application
 
+# # 创建Application对象，连接到正在运行的Typora进程
+# app = Application(backend='uia').connect(process=5092)
+#
+# # 获取与Typora相关的窗口对象，使用正则表达式匹配窗口标题
+# win = app.window(title_re='.*po.*')
+#
+# # 添加等待
+# win.wait('exists')
+#
+# # 最小化
+# win.minimize()
+# print("is_minimized:", win.is_minimized())
+#
+# win.maximize()
+# print("is_maximized:", win.is_maximized())
+#
+# win.close()
+
 # 创建Application对象，连接到正在运行的Typora进程
 app = Application(backend='uia').connect(process=5092)
 
 # 获取与Typora相关的窗口对象，使用正则表达式匹配窗口标题
 win = app.window(title_re='.*po.*')
 
-# 添加等待
+# 检查窗口是有效的句柄
 win.wait('exists')
 
-# 最小化
-win.minimize()
-print("is_minimized:", win.is_minimized())
+# 检查窗口是否可见
+win.wait('visible')
 
-win.maximize()
-print("is_maximized:", win.is_maximized())
+# 检查窗口是否未被禁用
+win.wait('enabled')
 
-win.close()
+# 检查窗口是否准备就绪
+win.wait('ready')
