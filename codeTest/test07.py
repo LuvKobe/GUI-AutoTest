bf5a9eaf68c60446026ff54ae2855bc8a278d23b
Edison <3078601436@qq.com>
2026-06-15 20:49:11 +0800

    add: 文本操作

--- codeTest/test07.py
+++ codeTest/test07.py
@@ -8,10 +8,27 @@ from pywinauto.application import Application
 # # 获取窗口的标题
 # print(win.texts())
 
+
+# # 打开Typora
+# app = Application(backend='uia').connect(process=8016)
+# win = app.window(title_re='.*po.*')
+# win.wait("visible") # 保证窗口是可见的
+#
+# file = win.child_window(title="文件(​F)", control_type="MenuItem")
+# print(file.texts())
+
+
+# # 打开Typora
+# app = Application(backend='uia').connect(process=8016)
+# win = app.window(title_re='.*po.*')
+# win.wait("visible") # 保证窗口是可见的
+#
+# print(win.window_text())
+
 # 打开Typora
 app = Application(backend='uia').connect(process=8016)
 win = app.window(title_re='.*po.*')
 win.wait("visible") # 保证窗口是可见的
 
-# 获取窗口的标题
-print(win.texts())
+file = win.child_window(title="文件(​F)", control_type="MenuItem")
+print(file.window_text())
