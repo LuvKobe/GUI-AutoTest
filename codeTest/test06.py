1faf6c4a484d4e4680b6f568ae08089df9cbea23
Edison <3078601436@qq.com>
2026-06-15 20:32:55 +0800

    add: 模拟鼠标右键单击操作

--- codeTest/test06.py
+++ codeTest/test06.py
@@ -2,34 +2,43 @@ import time
 
 from pywinauto.application import Application
 
-# 打开计算器
-app = Application(backend="uia").connect(process=14752)
-win = app.window(title="计算器")
+# # 打开计算器
+# app = Application(backend="uia").connect(process=14752)
+# win = app.window(title="计算器")
+# win.wait("visible") # 保证窗口是可见的
+# time.sleep(2)
+#
+# # 点击计算器上的按钮, 进行 1 + 2 = 3
+#
+# # 定位数字按钮1
+# btn_1 = win.child_window(title="一", auto_id="num1Button", control_type="Button")
+# # 点击数字按钮1
+# btn_1.click_input()
+# time.sleep(2)
+#
+# # 定位按钮+
+# add_btn = win.child_window(title="加", auto_id="plusButton", control_type="Button")
+# # 点击按钮+
+# add_btn.click_input()
+# time.sleep(2)
+#
+# # 定位数字按钮2
+# btn_2 = win.child_window(title="二", auto_id="num2Button", control_type="Button")
+# # 点击数字按钮2
+# btn_2.click_input()
+# time.sleep(2)
+#
+# # 定位按钮=
+# equal_btn = win.child_window(title="等于", auto_id="equalButton", control_type="Button")
+# # 点击按钮=
+# equal_btn.click_input()
+# time.sleep(5)
+
+
+# 打开Typora
+app = Application(backend='uia').connect(process=8016)
+win = app.window(title_re='.*po.*')
 win.wait("visible") # 保证窗口是可见的
-time.sleep(2)
-
-# 点击计算器上的按钮, 进行 1 + 2 = 3
-
-# 定位数字按钮1
-btn_1 = win.child_window(title="一", auto_id="num1Button", control_type="Button")
-# 点击数字按钮1
-btn_1.click_input()
-time.sleep(2)
-
-# 定位按钮+
-add_btn = win.child_window(title="加", auto_id="plusButton", control_type="Button")
-# 点击按钮+
-add_btn.click_input()
-time.sleep(2)
-
-# 定位数字按钮2
-btn_2 = win.child_window(title="二", auto_id="num2Button", control_type="Button")
-# 点击数字按钮2
-btn_2.click_input()
-time.sleep(2)
-
-# 定位按钮=
-equal_btn = win.child_window(title="等于", auto_id="equalButton", control_type="Button")
-# 点击按钮=
-equal_btn.click_input()
-time.sleep(5)
+print(win.print_control_identifiers())
+# 对窗口进行右键操作
+win.right_click_input()
