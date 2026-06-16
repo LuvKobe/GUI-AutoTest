b27e6e81eca4ffc5f2cabb3dc84832d850de9033
Edison <3078601436@qq.com>
2026-06-16 10:04:24 +0800

    add:

---
+++ codeTest/test09.py
@@ -0,0 +1,23 @@
+import time
+
+from pywinauto import mouse
+from pywinauto.application import Application
+
+#连接抖音
+app = Application(backend="uia").connect(process=13864)
+#定位窗口
+win = app.window(title="抖音")
+win.wait("visible")
+
+for i in range(0,3):
+    #双击视频刷赞
+    point = win.rectangle().mid_point()
+
+    time.sleep(2)
+    #调用高级api双击
+    win.double_click_input()
+    # #鼠标操作
+    # mouse.double_click(coords=(point.x,point.y))
+    #刷到下一个视频
+    time.sleep(2)
+    mouse.scroll(coords=(point.x,point.y),wheel_dist=-500)
