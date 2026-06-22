313f020cb62baeadc7e9b10f408413ac213508e6
Edison <3078601436@qq.com>
2026-06-22 14:24:48 +0800

    add: 获取列表视图中的所有项目

---
+++ codeTest/test14.py
@@ -0,0 +1,12 @@
+from pywinauto.application import Application
+
+# 连接记事本窗口
+app = Application(backend="uia").connect(process=7348)
+win = app.window(title_re='.*文件资源管理器')
+win.wait("visible") # 保证窗口是可见的
+
+# 定位列表控件
+list_ctrol = win.child_window(title="项目视图", control_type="List")
+
+# 打印子控件信息
+print(list_ctrol.get_items())
