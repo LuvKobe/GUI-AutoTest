2ac2832ab09932dc92697144ace4c42d87bc4f4b
Edison <3078601436@qq.com>
2026-06-22 10:55:02 +0800

    add: menu_select

--- codeTest/test12.py
+++ codeTest/test12.py
@@ -18,6 +18,21 @@ from pywinauto.application import Application
 # print(menu_bar.item_by_index(4))
 
 
+# # 连接记事本窗口
+# app = Application(backend="uia").connect(process=14940)
+# win = app.window(title_re='.*记事本.*')
+# win.wait("visible") # 保证窗口是可见的
+#
+# #win.print_control_identifiers()
+#
+# # 获取所有的菜单项
+# menu_bar = win.child_window(title="应用程序", auto_id="MenuBar", control_type="MenuBar")
+# #print(menu_bar.items())
+#
+# # 选择保存文件
+# menu_bar.item_by_path(path="文件 -> 另存为").click_input()
+
+
 # 连接记事本窗口
 app = Application(backend="uia").connect(process=14940)
 win = app.window(title_re='.*记事本.*')
@@ -30,4 +45,4 @@ menu_bar = win.child_window(title="应用程序", auto_id="MenuBar", control_typ
 #print(menu_bar.items())
 
 # 选择保存文件
-menu_bar.item_by_path(path="文件 -> 另存为").click_input()
+win.menu_select(path="文件 -> 另存为")
