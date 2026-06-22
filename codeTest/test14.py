2d8c0fbaa5783f268b0d378794763e437ba946e1
Edison <3078601436@qq.com>
2026-06-22 14:26:57 +0800

    add: 获取列表视图中的项数

--- codeTest/test14.py
+++ codeTest/test14.py
@@ -1,5 +1,16 @@
 from pywinauto.application import Application
 
+# # 连接记事本窗口
+# app = Application(backend="uia").connect(process=7348)
+# win = app.window(title_re='.*文件资源管理器')
+# win.wait("visible") # 保证窗口是可见的
+#
+# # 定位列表控件
+# list_ctrol = win.child_window(title="项目视图", control_type="List")
+
+# 打印子控件信息
+#print(list_ctrol.get_items())
+
 # 连接记事本窗口
 app = Application(backend="uia").connect(process=7348)
 win = app.window(title_re='.*文件资源管理器')
@@ -8,5 +19,5 @@ win.wait("visible") # 保证窗口是可见的
 # 定位列表控件
 list_ctrol = win.child_window(title="项目视图", control_type="List")
 
-# 打印子控件信息
-print(list_ctrol.get_items())
+#打印列表项数量
+print("items:",list_ctrol.item_count())
