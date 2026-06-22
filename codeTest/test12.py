2b51908a18ac976d8d094df98874f98449ab0e63
Edison <3078601436@qq.com>
2026-06-22 10:37:04 +0800

    add: item_by_index

---
+++ codeTest/test12.py
@@ -0,0 +1,18 @@
+from pywinauto.application import Application
+
+# 连接记事本窗口
+app = Application(backend="uia").connect(process=14940)
+win = app.window(title_re='.*记事本.*')
+win.wait("visible") # 保证窗口是可见的
+
+#win.print_control_identifiers()
+
+# 获取所有的菜单项
+menu_bar = win.child_window(title="应用程序", auto_id="MenuBar", control_type="MenuBar")
+#print(menu_bar.items())
+
+print(menu_bar.item_by_index(0))
+print(menu_bar.item_by_index(1))
+print(menu_bar.item_by_index(2))
+print(menu_bar.item_by_index(3))
+print(menu_bar.item_by_index(4))
